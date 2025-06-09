3ff7f0704fb8b9d512d44bf2a4c0c1ea00f76852
Krishna Malviya <malviyakrishna1108@gmail.com>
2025-06-09 15:18:12 +0530

    changes

--- lottery/src/App.jsx
+++ lottery/src/App.jsx
@@ -1,13 +1,19 @@
 
-import './App.css'
-import Lottery from './Lottery'
+import './App.css';
+import Lottery from './Lottery';
+import TicketNum from './TicketNum';
+
 
 function App() {
   
 
   return (
     <>
-     <Lottery/>
+     <Lottery n ={3} winningSum={15}/>
+
+     
+
+
     </>
   )
 }
